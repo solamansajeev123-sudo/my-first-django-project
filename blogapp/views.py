5da2b68f3adc3a869c7e8ab50f277edb87a60ffb
Solaman <solamansajeev123@gmail.com>
2026-03-25 10:23:13 +0530

    Updated my project

--- blogapp/views.py
+++ blogapp/views.py
@@ -3,15 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 from dataApp.models import Post
 
-def create_post(request):
-    if request.method == 'POST':
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-
-        Post.objects.create(
-            title=title,
-            content=content,
-            author=request.user
-        )
-
-    return render(request, 'hello.html')
+def home(request):
+    if request.method=='POST':
+        title=request.POST.get('title')
+        content=request.POST.get('content')
+        Post.objects.create(title=title,content=content,author=request.user)
+    return render(request,'hello.html')
